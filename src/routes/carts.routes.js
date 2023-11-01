7cd81e9ea1507991d8b003cb53949fe2a9321794
luferrduc <lu.ferrando@duocuc.cl>
2023-11-01 13:15:52 -0300

    Arreglo ruta carts

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -1,110 +1,55 @@
 import { Router } from "express";
-// import ProductManager from "../dao/fileManagers/product-file.manager.js";
+// import CartManager from "../dao/fileManagers/cart-file.manager.js";
+import CartsManager from "../dao/dbManagers/carts.manager.js";
 import ProductManager from "../dao/dbManagers/products.manager.js";
 
-import { productsFilePath } from "../utils.js";
+import { cartsFilePath } from "../utils.js";
 
 const router = Router();
-// const manager = new ProductManager(productsFilePath);
-const manager = new ProductManager();
-
+// const manager = new CartManager(cartsFilePath);
+const manager = new CartsManager();
+const productManager = new ProductManager();
 router
-  .get("/", async (req, res) => {
+  .get("/:cid", async (req, res) => {
+    const cid = req.params.cid;
     try {
-      const { limit } = req.query;
-      const products = await manager.getAll();
-      if (!products)
-        return res.status(200).send({ status: "success", payload: [] });
-      // Limit validations
-      if (!limit || parseInt(limit) > products.length)
-        return res.send({ status: "success", payload: products });
-      if (parseInt(limit) < 0)
+      const cart = await manager.getById(cid);
+      if (!cart)
         return res
           .status(400)
-          .send({ status: "error", message: "Limit must be a positve number" });
+          .send({ status: "error", error: "Cart not found" });
 
-      const filteredProducts = products.slice(0, parseInt(limit));
-
-      return res.send({ status: "success", payload: filteredProducts });
-    } catch (error) {
-      return res.status(500).send({ status: "error", error: error.message });
-    }
-  })
-
-  .get("/:pid", async (req, res) => {
-    try {
-      const { pid } = req.params;
-      const product = await manager.getById(pid);
-      if (!product)
-        return res.status(404).send({ status: "error", error: "Product not found" });
-
-      return res.send({ status: "success", payload: product });
+      return res.send({ status: "success", payload: cart });
     } catch (error) {
       return res.status(500).send({ status: "error", error: error.message });
     }
   })
   .post("/", async (req, res) => {
-
     try {
-      const product = req.body;
-      const io = req.app.get("socketio");
-      const { title, description, price, thumbnail, code, stock, status } =
-        product;
-      if (!title || !description || !price || !code || !stock)
-        return res
-          .status(400)
-          .send({ status: "error", error: "Incomplete values" });
-      const newProduct = await manager.create(product);
-      io.emit("refreshProducts", await manager.getAll());
-      return res.send({ status: "success", payload: newProduct });
+      const cart = await manager.create();
+      return res.send({ status: "success", payload: cart });
     } catch (error) {
       return res.status(500).send({ status: "error", error: error.message });
     }
   })
-  .put("/:pid", async (req, res) => {
+  .post("/:cid/product/:pid", async (req, res) => {
     try {
-      const { pid } = req.params;
-      const product = req.body;
-      const { title, description, price, thumbnail, code, stock, status } =
-        product;
-      if (!title || !description || !price || !code || !stock)
-        return res
-          .status(400)
-          .send({ status: "error", error: "Incomplete values" });
-
-      const productExists = await manager.getById(pid);
-      if (!productExists)
+      const { cid, pid } = req.params;
+      const product = await productManager.getById(pid)
+      if (!product)
         return res
           .status(404)
-          .send({ status: "error", error: "Product not found, incorrect id" });
+          .send({ status: "error", error: "Product not found" });
 
-      const updatedProduct = await manager.update(pid, product);
-      return res.send({
-        status: "success",
-        payload:  updatedProduct,
-      });
-    } catch (error) {
-      return res.status(500).send({ status: "error", error: error.message });
-    }
-  })
-  .delete("/:pid", async (req, res) => {
-    try {
-      const { pid } = req.params;
-     
-      const io = req.app.get("socketio");
-      const deletedProduct = await manager.delete(pid);
-      if (deletedProduct.deletedCount === 0)
+      const cart = await manager.addProduct(cid, pid);
+      if (!cart)
         return res
-          .status(400)
-          .send({ status: "error", error: "Product not found, incorrect id" });
-  
-      io.emit("refreshProducts", await manager.getAll());
-      return res.send({ status: "success", payload: "Product deleted succesfully" });
+          .status(404)
+          .send({ status: "error", error: "Cart or product not found" });
+      return res.send({ status: "success", payload: cart });
     } catch (error) {
       return res.status(500).send({ status: "error", error: error.message });
-      
     }
-   
   });
 
 export default router;
